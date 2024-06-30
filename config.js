cd107589bbf937344e1c6764b3fc922ea1f818e7
mqr270 <hurairahmadtech270@gmail.com>
2024-06-30 13:04:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ global.waPresence = process.env.WAPRESENCE || "recording";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.read_status = process.env.AUTO_READ_STATUS || "false";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
@@ -56,18 +56,18 @@ global.scan = "https://pair-qr-wasi-md.onrender.com";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUVEaWJGV1BzVTRrbEtjMXRxb0VWeEx2VzdwaVVicEl1Y3ViMHhrNTNuWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNmR5b1JTV2ZKQzNWY2NSZC9YMkdQVElBaTViWlVkenhxS0pJcUIzdFl4VT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIyT2ZsZ3JCM21TcHBwY0llZnhjcC9uUGcweUFmSlZsSlFhTm9nVEV5RW1jPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjWVdsSHN4dFNVZ3E1YUs4TDI4bWdBK2h5QzZPR05tNXp3T3BBay9YZnpjPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJKV2xXc1F6SThVM3dRaG5VSlR4Rm9PMEJFYzlhd2dRbWl3bTErcEIzVkk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImtzaFE4cDJGQlptblcyc2ZOK2lYRlZzeng1UTVHd3pYNkVXKzlkWm9tQjA9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY0laUVVRbU9QQm1QbGVwTE4zc3dQVlpEOVJLWmZZNkdmRUxZYnZFREFrUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTkdzWlJyckl2TldOSUswN3VPc0YxY0dJM3duN2hXRGYrQjkrU054ZndRZz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkhacDNhcm1IQWRHOTBSS2dVOXhWOGFCbHJhNUwwQnBmLzJqNERKMjhBWWFqR2JOR0tpb0pHVlVTU3ZpN21ncXhJVnZycFNEQUhTNDRla1d3c0RIU0JBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjQsImFkdlNlY3JldEtleSI6InpLRm5JY0JDdW9wNkhockZ5MmFoeVoyUDExTXNMRW42N2c0MUJVdzFJMFU9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IlpFMHhLUG5DVEZlQmlxZmo3OTB1VlEiLCJwaG9uZUlkIjoiMjE0MmZkZmMtOTNkYS00M2MzLTlmYjQtNzk2ZDY4NTM0YmZhIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFsNWdCbm8wMG8zVDRRNFlCWUZSb29GWEhWOD0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJEQkszVnZkcGxmOUxYOVpJeTJQa0twZkFudE09In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiTkRaSktQM0wiLCJtZSI6eyJpZCI6IjkyMzIxOTc1MzI3MDoyOEBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTjd2aUlJRUVKVDlnclFHR0FNZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiNTk3c1c2S3R5Z1Q3bHkyWFVXOVVRQThzZnljZFFseXNoSFBMTENPZS9oRT0iLCJhY2NvdW50U2lnbmF0dXJlIjoidHZCN0tjWGo4SHFnOUpEWnhUQlh0VUJDRTRyeVlhV2xSQ3hJbEh4emt5VGFob0ZTY0VXbzloejhJaDR6WVVObEZ2c3BaTFRoc1UybXpVU1FMYjZsQ1E9PSIsImRldmljZVNpZ25hdHVyZSI6InFNNFVWTmdreGNZMmU1LzZiUitsdUJ0M2lWUytCU2dMblltc2gwOFFmV3dqMW1OY0EveXYwTjYxMDR1SldGUTlFWjM5V0IzaGdESUMyUlB2MGExK0JBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTIzMjE5NzUzMjcwOjI4QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmVmZTdGdWlyY29FKzVjdGwxRnZWRUFQTEg4bkhVSmNySVJ6eXl3am52NFIifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MTk3MTM0NDEsIm15QXBwU3RhdGVLZXlJZCI6IkFCc0FBSEpDIn0="
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "𝐅𝐈𝐋𝐄-𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃-𝐁𝐘-𝐖𝐀𝐒𝐈-𝐌𝐃-𝐕2😍",
+  caption: process.env.CAPTION || "𝐅𝐈𝐋𝐄-𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃-𝐁𝐘- PAK News Media Group",
   author: process.env.PACK_AUTHER || "WASO",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "𝐖𝐀𝐒𝐈-𝐌𝐃-𝐕2",
-  ownername: process.env.OWNER_NAME || "WASI",
-  errorChat: process.env.ERROR_CHAT || "923192173398",
+  botname: process.env.BOT_NAME || "MQR",
+  ownername: process.env.OWNER_NAME || "MQR",
+  errorChat: process.env.ERROR_CHAT || "923219753270",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
